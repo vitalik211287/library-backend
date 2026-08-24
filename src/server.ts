570bbaf9b4683fcadaf8d07c4064ac4d0944d192
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2026-08-24 12:54:14 +0300

    z

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 
-const PORT = 4000;
+const PORT = Number(process.env.PORT) || 4000;
 
-app.listen(PORT, ()=>{
-     console.log(`Server is running on http://localhost:${PORT}`);
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
