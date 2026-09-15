523ecb289393c19b557c0c6c6f758f7af401cc74
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2026-09-15 08:42:46 +0300

    fix: restore getBookById repository method

--- src/modules/books/repositories/booksRepository.ts
+++ src/modules/books/repositories/booksRepository.ts
@@ -4,6 +4,14 @@ export const getAllBooks = async () => {
   return prisma.book.findMany();
 };
 
+export const getBookById = async (bookId: string) => {
+  return prisma.book.findUnique({
+    where: {
+      id: bookId,
+    },
+  });
+};
+
 export const getBookByIsbn = async (isbn: string) => {
   return prisma.book.findUnique({
     where: {
